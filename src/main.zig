df6271ba324ace363a0007aa089244b15ee66281
TeamMeng <862915265@qq.com>
2025-04-19 20:26:05 +0800

    feature: support pointer

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,23 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig_dome_lib");
+pub fn main() !void {
+    var interger: i16 = 666;
+    const ptr = &interger;
+    ptr.* = ptr.* + 1;
+
+    // 667
+    std.debug.print("{}\n", .{interger});
+
+    const array = [_]i32{ 1, 2, 3, 4 };
+    const arr_ptr: [*]const i32 = &array;
+    // 1
+    std.debug.print("The first value is {}\n", .{arr_ptr[0]});
+
+    var p_array: [5]u8 = "Hello".*;
+
+    const array_pointer = &p_array;
+    try std.testing.expect(array_pointer.len == 5);
+
+    const slice: []u8 = p_array[1..3];
+    try std.testing.expect(slice.len == 2);
+}
