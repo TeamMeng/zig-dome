75b2007624e4ea3646f8d9726531b4135e7b38cf
TeamMeng <862915265@qq.com>
2026-06-23 19:05:34 +0800

    feat: support for and while add 1 to 100

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,20 @@
 const std = @import("std");
 
 pub fn main() !void {}
+
+test "while" {
+    var idx: i32 = 0;
+    var i: i32 = 0;
+    while (idx < 100) : (idx += 1) {
+        i += idx;
+    }
+    std.debug.print("i: {}\n", .{i});
+}
+
+test "for" {
+    var i: i32 = 0;
+    for (0..100) |val| {
+        i += @intCast(val);
+    }
+    std.debug.print("i: {}\n", .{i});
+}
