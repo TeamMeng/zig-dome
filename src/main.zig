c4f0e67943986d7753e4167925390fa81c61a156
TeamMeng <862915265@qq.com>
2026-06-24 09:05:52 +0800

    feat: support const and var

--- src/main.zig
+++ src/main.zig
@@ -18,3 +18,27 @@ test "for" {
     }
     std.debug.print("i: {}\n", .{i});
 }
+
+test "const" {
+    const pi: f32 = 3.14159;
+    const message = "Hello, zig!";
+
+    std.debug.print("Pi: {d:.5}, message: {s}\n", .{ pi, message });
+}
+
+test "var" {
+    var count: i32 = 0;
+    var score: i32 = 100;
+
+    count += 100;
+    score = 95;
+    std.debug.print("count: {}, score: {}\n", .{ count, score });
+}
+
+test "my_age_and_name" {
+    const name = "Meng";
+    var age: u8 = 23;
+
+    age += 1;
+    std.debug.print("name: {s}, next year age: {d}\n", .{ name, age });
+}
