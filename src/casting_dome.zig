e875edb7e5e8e84ce8cc5eeacb37ee72677d95f6
TeamMeng <862915265@qq.com>
2026-07-09 12:16:09 +0800

    feat: support cast

---
+++ src/casting_dome.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+test "type coercion - variable declaration" {
+    const a: u8 = 1;
+    const b: u16 = a;
+    _ = b;
+}
+
+test "type coercion - function call" {
+    const a: u8 = 1;
+    foo(a);
+}
+
+fn foo(b: u16) void {
+    _ = b;
+}
+
+test "type coercion - @as builtin" {
+    const a: u8 = 1;
+    const b = @as(u16, a);
+    _ = b;
+}
+
+test "integer widening" {
+    const a: u8 = 250;
+    const b: u16 = a;
+    const c: u32 = b;
+    const d: u64 = c;
+    const e: u64 = d;
+    const f: u128 = e;
+    try std.testing.expectEqual(f, a);
+}
+
+test "implicit unsigned integer to signed integer" {
+    const a: u8 = 250;
+    const b: i16 = a;
+    try std.testing.expectEqual(250, b);
+}
+
+test "float widening" {
+    const a: f16 = 12.34;
+    const b: f32 = a;
+    const c: f64 = b;
+    const d: f128 = c;
+    try std.testing.expectEqual(d, a);
+}
--- src/root.zig
+++ src/root.zig
@@ -14,3 +14,4 @@ pub const day_eight = @import("day-8.zig");
 pub const day_night = @import("day-9.zig");
 pub const day_ten = @import("day-10.zig");
 pub const error_dome = @import("error.zig");
+pub const casting_dome = @import("casting_dome.zig");
