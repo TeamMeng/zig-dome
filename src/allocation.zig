6e20c9e64e248ca3f8deba2f83a6434c27aece7b
TeamMeng <862915265@qq.com>
2026-07-13 22:47:57 +0800

    feat: support allocator

--- src/allocation.zig
+++ src/allocation.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 test "cli_allocation" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -9,4 +10,19 @@ test "cli_allocation" {
     const ptr = try allocator.create(i32);
     ptr.* = 123;
     std.debug.print("ptr: {d}\n", .{ptr.*});
+}
+
+test "using an allocator" {
+    var buffer: [100]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const result = try concat(allocator, "foo", "bar");
+    try std.testing.expectEqualStrings("foobar", result);
+}
+
+fn concat(allocator: Allocator, a: []const u8, b: []const u8) ![]u8 {
+    const result = try allocator.alloc(u8, a.len + b.len);
+    @memcpy(result[0..a.len], a);
+    @memcpy(result[a.len..], b);
+    return result;
 }
